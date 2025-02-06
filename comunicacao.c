ac4160739e584f34107aedf4364db6294fa5b016
sansaocarrasco <joao.pedro_jua@hotmail.com>
2025-02-06 18:58:38 -0300

    update: insert numbers in matriz

--- comunicacao.c
+++ comunicacao.c
@@ -29,16 +29,16 @@ volatile uint32_t last_press_time_B = 0;
 volatile char ultimo_caractere = ' ';
 volatile int numero_atual = -1;
 
-void npInit(uint pin) {
-    uint offset = pio_add_program(pio0, &ws2818b_program);
-    np_pio = pio0;
-    sm = pio_claim_unused_sm(np_pio, false);
-    if (sm < 0) {
-        np_pio = pio1;
-        sm = pio_claim_unused_sm(np_pio, true);
-    }
-    ws2818b_program_init(np_pio, sm, offset, pin, 800000.f);
-}
+// Definição de pixel GRB
+struct pixel_t
+{
+  uint8_t G, R, B; // Três valores de 8-bits compõem um pixel.
+};
+typedef struct pixel_t pixel_t;
+typedef pixel_t npLED_t; // Mudança de nome de "struct pixel_t" para "npLED_t" por clareza.
+
+// Declaração do buffer de pixels que formam a matriz.
+npLED_t leds[LED_COUNT];
 
 void npSetLED(const uint index, const uint8_t r, const uint8_t g, const uint8_t b) {
     pio_sm_put_blocking(np_pio, sm, g);
@@ -49,6 +49,59 @@ void npSetLED(const uint index, const uint8_t r, const uint8_t g, const uint8_t 
 void npClear() {
     for (uint i = 0; i < LED_COUNT; i++)
         npSetLED(i, 0, 0, 0);
+}
+
+void npInit(uint pin)
+{
+
+  // Cria programa PIO.
+  uint offset = pio_add_program(pio0, &ws2818b_program);
+  np_pio = pio0;
+
+  // Toma posse de uma máquina PIO.
+  sm = pio_claim_unused_sm(np_pio, false);
+  if (sm < 0)
+  {
+    np_pio = pio1;
+    sm = pio_claim_unused_sm(np_pio, true); // Se nenhuma máquina estiver livre, panic!
+  }
+
+  // Inicia programa na máquina PIO obtida.
+  ws2818b_program_init(np_pio, sm, offset, pin, 800000.f);
+
+  // Limpa buffer de pixels.
+  for (uint i = 0; i < LED_COUNT; ++i)
+  {
+    leds[i].R = 0;
+    leds[i].G = 0;
+    leds[i].B = 0;
+  }
+}
+
+int getIndex(int x, int y)
+{
+  // Se a linha for par (0, 2, 4), percorremos da esquerda para a direita.
+  // Se a linha for ímpar (1, 3), percorremos da direita para a esquerda.
+  if (y % 2 == 0)
+  {
+    return 24 - (y * 5 + x); // Linha par (esquerda para direita).
+  }
+  else
+  {
+    return 24 - (y * 5 + (4 - x)); // Linha ímpar (direita para esquerda).
+  }
+}
+
+void npWrite()
+{
+  // Escreve cada dado de 8-bits dos pixels em sequência no buffer da máquina PIO.
+  for (uint i = 0; i < LED_COUNT; ++i)
+  {
+    pio_sm_put_blocking(np_pio, sm, leds[i].G);
+    pio_sm_put_blocking(np_pio, sm, leds[i].R);
+    pio_sm_put_blocking(np_pio, sm, leds[i].B);
+  }
+  sleep_us(100); // Espera 100us, sinal de RESET do datasheet.
 }
 
 void npWriteNumber(int num) {
@@ -89,6 +142,17 @@ void button_callback(uint gpio, uint32_t events) {
     }
 }
 
+// Função pra desenhar a matriz na matriz de LEDs.
+void npDrawMatrix(int matriz[5][5][3]) {
+    for (int linha = 0; linha < 5; linha++) {
+        for (int coluna = 0; coluna < 5; coluna++) {
+            int posicao = getIndex(linha, coluna);
+            npSetLED(posicao, matriz[coluna][linha][0], matriz[coluna][linha][1], matriz[coluna][linha][2]);
+        }
+    }
+    npWrite();
+}
+
 int main() {
     stdio_init_all();
     i2c_init(I2C_PORT, 400 * 1000);
@@ -121,6 +185,8 @@ int main() {
                     npWriteNumber(numero_atual);
                 }
                 atualizar_display();
+                npDrawMatrix(matrizes[numero_atual]); // Usa o índice para acessar a matriz correta
+
             }
         }
         sleep_ms(100);
