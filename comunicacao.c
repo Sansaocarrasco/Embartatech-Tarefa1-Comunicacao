c6895810c706f8cbb0fbda0e7068ffbc667b2d01
sansaocarrasco <joao.pedro_jua@hotmail.com>
2025-02-06 17:59:37 -0300

    update: insert all

--- comunicacao.c
+++ comunicacao.c
@@ -48,7 +48,7 @@ int main()
     ssd1306_fill(&ssd, !cor); // Limpa o display
     ssd1306_rect(&ssd, 3, 3, 122, 58, cor, !cor); // Desenha um retângulo
     ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 10); // Desenha uma string
-    ssd1306_draw_string(&ssd, "abcdefg", 20, 30); // Desenha uma string
+    ssd1306_draw_string(&ssd, "abcdefghijklmnopqrstuvwxyz", 20, 30); // Desenha uma string
     ssd1306_draw_string(&ssd, "PROF WILTON", 15, 48); // Desenha uma string      
     ssd1306_send_data(&ssd); // Atualiza o display
 
